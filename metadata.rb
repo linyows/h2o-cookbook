0501ab35c637429b0a58452225b64edf7e205181
linyows <linyows@gmail.com>
2018-09-24 22:52:43 +0900

    bump version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'linyows@gmail.com'
 license          'MIT'
 description      'Installs/Configures h2o'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'build-essential'
 recipe 'h2o', 'Installs and configures h2o'
